e06c7de25903a68e61fdccd8720a11e05a90e1a3
Frederico Lopes <fmc.lopes@campus.fct.unl.pt>
2020-10-03 17:24:15 +0100

    second comit

--- scc2021-backend-aula1/src/scc/srv/MediaResource.java
+++ scc2021-backend-aula1/src/scc/srv/MediaResource.java
@@ -2,6 +2,12 @@ package scc.srv;
 
 import scc.utils.Hash;
 
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+
 import javax.ws.rs.Consumes;
 import javax.ws.rs.GET;
 import javax.ws.rs.POST;
@@ -9,25 +15,42 @@ import javax.ws.rs.Path;
 import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
 
 @Path("/media")
-public class MediaResource
-{
+public class MediaResource {
 
 	@POST
 	@Path("/")
 	@Consumes(MediaType.APPLICATION_OCTET_STREAM)
 	@Produces(MediaType.APPLICATION_JSON)
 	public String upload(byte[] contents) {
-		// TODO: replace by proper code
-		return "Some id";
+		String id = Hash.of(contents);
+		File file = new File(id);
+		try {
+			FileOutputStream fop = new FileOutputStream(file);
+			fop.write(contents);
+			fop.flush();
+			fop.close();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+		return id;
 	}
 
 	@GET
 	@Path("/{id}")
 	@Produces(MediaType.APPLICATION_OCTET_STREAM)
 	public byte[] download(@PathParam("id") String id) {
-		// TODO: replace by proper code
+		File file = new File(id);
+		try {
+			
+			
+		} catch (FileNotFoundException e) {
+			e.printStackTrace();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
 		return "Some text".getBytes();
 	}
 
