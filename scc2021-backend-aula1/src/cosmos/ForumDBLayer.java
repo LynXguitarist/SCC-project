67284b03bf115eebc754f440bcda0efa56d50415
rubenfjs <rubenfjs.94@gmail.com>
2020-10-26 09:11:08 +0000

    forum DB Layer sem publicações/respostas

--- scc2021-backend-aula1/src/cosmos/ForumDBLayer.java
+++ scc2021-backend-aula1/src/cosmos/ForumDBLayer.java
@@ -1,5 +1,16 @@
 package cosmos;
 
+import com.azure.cosmos.*;
+import com.azure.cosmos.models.CosmosItemRequestOptions;
+import com.azure.cosmos.models.CosmosItemResponse;
+import com.azure.cosmos.models.CosmosQueryRequestOptions;
+import com.azure.cosmos.models.PartitionKey;
+import com.azure.cosmos.util.CosmosPagedIterable;
+import data.Entity;
+import data.Forum;
+
+import java.util.Locale;
+
 public class ForumDBLayer {
 
 	// DATABASE
@@ -8,4 +19,63 @@ public class ForumDBLayer {
 	private static final String DB_NAME = "sc42764DB";
 	private static final String DB_CONTAINER = "forums";
 
+	private static EntityDBLayer instance;
+	private CosmosClient client;
+	private CosmosDatabase db;
+	private CosmosContainer forums;
+
+	public ForumDBLayer(CosmosClient client){
+		this.client = client;
+	}
+
+	public static synchronized EntityDBLayer getInstance() {
+		Locale.setDefault(Locale.US);
+		if (instance != null)
+			return instance;
+
+		CosmosClient client = new CosmosClientBuilder().endpoint(CONNECTION_URL).key(DB_KEY).directMode()
+				.consistencyLevel(ConsistencyLevel.SESSION).connectionSharingAcrossClientsEnabled(true)
+				.contentResponseOnWriteEnabled(true).buildClient();
+		instance = new EntityDBLayer(client);
+		return instance;
+
+	}
+
+	private synchronized void init() {
+		if (db != null)
+			return;
+		db = client.getDatabase(DB_NAME);
+		forums = db.getContainer(DB_CONTAINER);
+	}
+
+	public CosmosItemResponse<Forum> createForum(Forum forum) {
+		init();
+		return forums.createItem(forum);
+	}
+
+	public CosmosItemResponse<Forum> putForum(Forum forum) {
+		init();
+		return forums.replaceItem(forum, forum.getId(), new PartitionKey(forum.getId()),
+				new CosmosItemRequestOptions());
+	}
+
+	public CosmosPagedIterable<Forum> getForumById(String id) {
+		init();
+		return forums.queryItems("SELECT * FROM forums WHERE forums.id=\"" + id + "\"",
+				new CosmosQueryRequestOptions(), Forum.class);
+	}
+
+	public CosmosPagedIterable<Forum> getForums() {
+		init();
+		return forums.queryItems("SELECT * FROM forums ", new CosmosQueryRequestOptions(), Forum.class);
+	}
+
+	public CosmosItemResponse<Object> delForum(String id) {
+		init();
+		return forums.deleteItem(id, new PartitionKey(id), new CosmosItemRequestOptions());
+	}
+
+	public void close() {
+		client.close();
+	}
 }
